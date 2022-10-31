b0b81866c12e6a33560022f6e37db267c640f564
noahdude1203 <gissn@oregonstate.edu>
2022-10-31 13:01:33 -0700

    Update access.js

--- access.js
+++ access.js
@@ -14,3 +14,29 @@ console.log("== photoCards:", photoCards)
 for(var i = 0; i < photoCards.length; i++){
     console.log(photoCards[i])
 }
+
+var images = document.getElementsByTagName("img")
+console.log("== images:", images)
+
+var rightNavItem = document.querySelector(".navitem.right")
+console.log("== rightNavItem:", rightNavItem)
+
+var allNavItems = document.querySelectorAll("li.navitem")
+console.log("== allNavItems:", allNavItems)
+
+var firstPhotoCard = photoCards[0]
+console.log("== firstPhotoCard.innerHTML:", firstPhotoCard.innerHTML)
+console.log("== firstPhotoCard.textContent:", firstPhotoCard.textContent)
+
+console.log("== images[0].src:", images[0].src)
+images[0].src = "http:/placekitten.com/400/400"
+console.log("== images[0].src:", images[0].src)
+
+var firstCaption = firstPhotoCard.getElementsByClassName("caption")[0]
+console.log("== firstCaption:", firstCaption)
+
+
+
+var newCaption = "<img src=x onerror='alert(\"You were Hacked\")>"
+//firstCaption.innerHTML = newCaption
+firstCaption.textContent = newCaption
